7e763db1bfdf5dbf6848dd06da45f8fe0281ed5b
xiaobiaohust <xiaobiao_my@163.com>
2021-05-31 22:28:50 +0800

    1

--- 76.最小覆盖子串.java
+++ 76.最小覆盖子串.java
@@ -7,6 +7,36 @@
 // @lc code=start
 class Solution {
     public String minWindow(String s, String t) {
+        char[]ss = s.toCharArray();
+        char[]tt = t.toCharArray();
+        int[]map = new int[256];
+        for(int i=0;i<tt.length;++i){
+            map[tt[i]]++;
+        }
+        int left = 0;
+        int right = 0;
+        int match = tt.length;
+        int minlen = Integer.MAX_VALUE;
+        int start = -1;
+        while(right<ss.length){
+            map[ss[right]]--;
+            if(map[ss[right]]>=0){
+                match--;
+            }
+            if(match==0){
+                while(map[ss[left]]<0){
+                    map[ss[left++]]++;
+                }
+                if(minlen>right-left+1){
+                    minlen = right-left+1;
+                    start = left;
+                }
+                map[ss[left++]]++;
+                match++;
+            }
+            right++;
+        }
+        return start==-1?"":s.substring(start, start+minlen);
 
     }
 }
