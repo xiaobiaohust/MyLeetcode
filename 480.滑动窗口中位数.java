93e3389cd6f200e9ada6981fa2bc3be14fffb317
xiaobiao <343003079@qq.com>
2021-06-01 19:06:15 +0800

    1

--- 480.滑动窗口中位数.java
+++ 480.滑动窗口中位数.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /*
  * @lc app=leetcode.cn id=480 lang=java
  *
@@ -7,8 +9,74 @@
 // @lc code=start
 class Solution {
     public double[] medianSlidingWindow(int[] nums, int k) {
+        double[] res = new double[nums.length - k + 1];
+        int[] window = new int[k];
+        //添加初始值
+        for (int i = 0; i < k; i++) {
+            window[i] = nums[i];
+        }
+        //初始的快排，懒得写直接调用
+        Arrays.sort(window);
+        res[0] = getMid(window);
+        //窗口滑动
+        for (int i = 0; i < nums.length - k; i++) {
+            //需要删除的数
+            int index = search(window, nums[i]);
+            //替换为需要插入的数
+            window[index] = nums[i + k];
+            //向后冒泡
+            while (index < window.length - 1 && window[index] > window[index + 1]) {
+                swap(window, index, index + 1);
+                index++;
+            }
+            //向前冒泡
+            while (index > 0 && window[index] < window[index - 1]) {
+                swap(window, index, index - 1);
+                index--;
+            }
+            res[i + 1] = getMid(window);
+        }
+        return res;
+    }
 
+    //交换
+    private void swap(int[] window, int i, int j) {
+        int temp = window[i];
+        window[i] = window[j];
+        window[j] = temp;
+    }
+
+    //求数组的中位数
+    private double getMid(int[] window) {
+        int len = window.length;
+        if (window.length % 2 == 0) {
+            //避免溢出
+            return window[len / 2] / 2.0 + window[len / 2 - 1] / 2.0;
+        } else {
+            return window[len / 2];
+        }
+    }
+
+    //最简单的二分查找
+    private int search(int[] window, int target) {
+        int start = 0;
+        int end = window.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (window[mid] > target) {
+                end = mid - 1;
+            } else if (window[mid] < target) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
     }
 }
+
+
+
+
 // @lc code=end
 
