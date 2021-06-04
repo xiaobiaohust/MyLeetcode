e683c3b3c415ead5b0cda49d226e412245a38a76
xiaobiao <343003079@qq.com>
2021-06-04 18:13:23 +0800

    1

--- 11.盛最多水的容器.java
+++ 11.盛最多水的容器.java
@@ -5,6 +5,12 @@
  */
 
 // @lc code=start
+/**
+ * 双指针
+ * 使用左右指针夹住中间区域，最多的水和最低的height有关
+ * 假设水有底和高决定，底从大到小，移动左右指针，每次移动指针所在位置小的指针，因为我们需要得到可能更大的数，而一旦移动
+ * 数量大的指针，高度一样，宽度变小了，不可能得到更大的面积
+ */
 class Solution {
     public int maxArea(int[] height) {
         int max = 0;
@@ -19,7 +25,6 @@ class Solution {
             }
         }
         return max;
-
     }
 }
 // @lc code=end
