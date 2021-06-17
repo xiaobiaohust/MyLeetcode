f386cac5bd8f6aabf5e64e2451a858344c2258e6
xiaobiao <343003079@qq.com>
2021-06-17 15:31:23 +0800

    5

---
+++ 438.找到字符串中所有字母异位词.java
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=438 lang=java
+ *
+ * [438] 找到字符串中所有字母异位词
+ */
+/**
+ * 使用哈希表去统计字符串
+ */
+// @lc code=start
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        
+
+    }
+}
+// @lc code=end
+
