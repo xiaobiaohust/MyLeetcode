92207a069f265ebe613ec234fb186e3b3637a2d4
xiaobiao <343003079@qq.com>
2021-06-18 17:58:06 +0800

    2

---
+++ 617.合并二叉树.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=617 lang=java
+ *
+ * [617] 合并二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+ * 二叉树的遍历
+ */
+class Solution {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if(root1==null||root2==null){
+            return root1!=null?root1:root2;
+        }
+
+        root1.val = root1.val+root2.val;
+        TreeNode left = mergeTrees(root1.left, root2.left);
+        TreeNode right = mergeTrees(root1.right, root2.right);
+        root1.left = left;
+        root1.right = right;
+        return root1;
+    }
+}
+// @lc code=end
+
---
+++ 621.任务调度器.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=621 lang=java
+ *
+ * [621] 任务调度器
+ */
+
+// @lc code=start
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+
+    }
+}
+// @lc code=end
+
